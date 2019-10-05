6fec1f58801d1e192954bce241284d0efe7ac056
Jake Schmitz <jschmitz@gmail.com>
2019-10-05 06:27:15 -0500

    Initial commit

---
+++ lib/minimal_server/router.ex
@@ -0,0 +1,18 @@
+defmodule MinimalServer.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(message()))
+  end
+
+  defp message do
+    %{
+      text: "Will send a text message"
+    }
+  end
+end
