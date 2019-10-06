9406b81d65568236fdf4524be670ce6bbfccbdf0
Jake Schmitz <jschmitz@gmail.com>
2019-10-06 16:43:45 -0500

    Add message capability

--- lib/minimal_server/router.ex
+++ lib/minimal_server/router.ex
@@ -11,8 +11,29 @@ defmodule MinimalServer.Router do
   end
 
   defp message do
+    persy_account = System.get_env("PERSY_ACCOUNT")
+    persy_token = System.get_env("PERSY_TOKEN")
+    from_phone = System.get_env("FROM_PHONE")
+    to_phone = System.get_env("TO_PHONE")
+
+    :inets.start()
+    :ssl.start()
+
+    {:ok, {_status, _headers, body}} =
+      :httpc.request(
+        :post,
+        {'https://www.persephony.com/apiserver/Accounts/#{persy_account}/Messages',
+         [
+           {'Authorization',
+            'Basic ' ++ :base64.encode_to_string('#{persy_account}:#{persy_token}')}
+         ], 'application/json',
+         '{"to":"#{to_phone}", "from":"#{from_phone}", "text":"test message from me"}'},
+        [],
+        []
+      )
+
     %{
-      text: "Will send a text message"
+      text: "#{body}"
     }
   end
 end
